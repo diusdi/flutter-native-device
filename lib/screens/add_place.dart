40befeb64b81affe94fffcc9b46d19ef05054064
Dion <riadidion@gmail.com>
2023-11-24 15:24:41 +0700

    first init

---
+++ lib/screens/add_place.dart
@@ -0,0 +1,69 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:native_device/providers/user_places.dart';
+import 'package:native_device/widgets/image_input.dart';
+
+class AddPlaceScreen extends ConsumerStatefulWidget {
+  const AddPlaceScreen({super.key});
+
+  @override
+  ConsumerState<AddPlaceScreen> createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+  File? _selectedImage;
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  void _savePlace() {
+    final enteredTitle = _titleController.text;
+
+    if (enteredTitle.isEmpty || _selectedImage == null) {
+      return;
+    }
+
+    ref.read(userPlacesProvider.notifier).addPlace(enteredTitle, _selectedImage!);
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add new place'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Column(children: [
+            TextField(
+                decoration: const InputDecoration(
+                  labelText: 'title',
+                ),
+                controller: _titleController,
+                style: TextStyle(
+                    color: Theme.of(context).colorScheme.onBackground)),
+            const SizedBox(height: 10),
+            ImageInput(
+              onPickImage: (image) {
+                _selectedImage = image;
+              },
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton.icon(
+                onPressed: _savePlace,
+                label: const Text('add'),
+                icon: const Icon(Icons.add))
+          ]),
+        ),
+      ),
+    );
+  }
+}
